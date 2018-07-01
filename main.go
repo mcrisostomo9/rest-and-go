b1c3213a6b6f63d1b3f69e478e43c62345d0bfa4
Mark Crisostomo <mark@deviseinteractive.com>
2018-07-01 14:43:37 -0700

    added routes

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"os"
+	"log"
+	"github.com/gorilla/handlers"
+	"net/http"
+	"rest-and-go/store"
+)
+
+func main() {
+	//get the PORT env variable
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		log.Fatal("$PORT must be set")
+	}
+
+	// create routes
+	router := store.NewRouter()
+
+	// These two lines are important if you're designing a front-end to utilise this API methods
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+
+	//Launch server with CORS validations
+	log.Fatal(http.ListenAndServe(":" + port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
+}
